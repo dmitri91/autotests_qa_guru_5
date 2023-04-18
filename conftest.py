4331c8150f4d496060d9ce20abbbd77913d01385
Dmitrii <dalariono1@example.com>
2023-04-18 09:27:11 +0300

    Changed  window size

--- conftest.py
+++ conftest.py
@@ -8,8 +8,8 @@ def web_browser():
     chrome_options = webdriver.ChromeOptions()
     # chrome_options.add_argument('--headless')
     browser.config.driver_options = chrome_options
-    browser.config.window_height = 1920
-    browser.config.window_width = 1080
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
     browser.config.base_url = 'https://demoqa.com'
     yield browser
     browser.quit()
